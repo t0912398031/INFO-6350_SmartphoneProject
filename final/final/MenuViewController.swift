f660d1f6b76f121b47f1dff58387fc4031e989cf
Chung-Yang Li <li.chun@ICD25RR0RXGG7V.tld>
2018-12-03 16:59:02 -0500

    123

--- final/final/MenuViewController.swift
+++ final/final/MenuViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
+
 protocol SlideMenuDelegate {
     func slideMenuItemSelectedAtIndex(_ index : Int32)
 }
 
 class MenuViewController: UIViewController {
+    
+    @IBOutlet weak var btnCloseMenuOverlay: UIButton!
     var btnMenu: UIButton!
     var delegate: SlideMenuDelegate?
+    let userDefaults = UserDefaults()
     
+    @IBAction func signOut(_ sender: Any) {
+        print("Sign out pressed")
+        do{
+            print("4")
+            try Auth.auth().signOut()
+            
+            try GIDSignIn.sharedInstance()?.signOut()
+            print("2")
+            self.userDefaults.removeObject(forKey: "usersignedin")
+            print("3")
+            self.userDefaults.synchronize()
+            self.dismiss(animated: true, completion: nil)
+        }
+        catch let error as NSError{
+            print(error.localizedDescription)
+        }
+        
+    }
+    @IBAction func btnCloseTapped(_ sender: UIButton) {
+        btnMenu.tag = 0
+        
+        if (self.delegate != nil) {
+            var index = Int32(sender.tag)
+            if(sender == self.btnCloseMenuOverlay){
+                index = -1
+            }
+            delegate?.slideMenuItemSelectedAtIndex(index)
+        }
+        
+        UIView.animate(withDuration: 0.3, animations: { () -> Void in
+            self.view.frame = CGRect(x: UIScreen.main.bounds.size.width, y: 0, width: UIScreen.main.bounds.size.width,height: UIScreen.main.bounds.size.height)
+            self.view.layoutIfNeeded()
+            self.view.backgroundColor = UIColor.clear
+        }, completion: { (finished) -> Void in
+            self.view.removeFromSuperview()
+            self.removeFromParent()
+        })
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
